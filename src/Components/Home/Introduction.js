1eea3ead362008f39408da99f3206c3be82c09e4
quynhtran158 <quynhtran.3002@gmail.com>
2024-02-03 20:39:31 -0500

    first commit

---
+++ src/Components/Home/Introduction.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Introduction extends Component {
+  render() {
+    return (
+      <div className="introduction-container px-5 py-5">
+        <h1>Welcome to "name" website</h1>
+        <p>
+          This is a simple website that I built using React. It has a few pages
+          and a few components. It's not much, but it's a start.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Introduction;
---
+++ src/Components/Home/Support.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Support extends Component {
+  render() {
+    return (
+      <div className="support-container px-5 py-5">
+        <h1 className="py-1">We Support</h1>
+        <p>
+          If you need help, please contact us at:
+          <br />
+          Email:
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Support;
