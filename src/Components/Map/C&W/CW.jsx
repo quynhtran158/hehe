2a8d20a2a8dea83ebd87733cb8f0e0b85738b814
quynhtran158 <quynhtran.3002@gmail.com>
2024-02-03 22:22:29 -0500

    map cw

---
+++ src/Components/Map/C&W/CW.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class CW extends Component {
+  render() {
+    return (
+      <div className="childrenMap">
+        <iframe
+          src="https://www.google.com/maps/d/u/0/embed?mid=1b9HZPFYh9EAR4k_xT7KEZLk9Wkd80nQ&ehbc=2E312F"
+          width="1000"
+          height="600"
+          frameBorder="0"
+          allowFullScreen></iframe>
+      </div>
+    );
+  }
+}
+
+export default CW;
